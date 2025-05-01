6954e52cae278a87b19abd675a11864513e5722a
yash <yash.mahadevwala@toodleteam.com>
2025-05-01 14:51:18 +0530

    update backend db config

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,16 +4,23 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+let db_name= (process.env.NODE_ENV === "production" ? process.env.PROD_DB_NAME : process.env.DB_NAME);
+let db_user= (process.env.NODE_ENV === "production" ? process.env.PROD_DB_USER : process.env.DB_USER);
+let db_password= (process.env.NODE_ENV === "production" ? process.env.PROD_DB_PASSWORD : process.env.DB_PASSWORD);
+let db_host= (process.env.NODE_ENV === "production" ? process.env.PROD_DB_HOST : process.env.DB_HOST);
+let db_port = (process.env.NODE_ENV === "production" ? process.env.PROD_DB_PORT : 3306);
+
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  db_name,
+  db_user,
+  db_password,
   {
-    host: process.env.DB_HOST,
+    host: db_host,
+    port: db_port,
     dialect: "mysql",
     logging: false,
     pool: {
-      max: 5,
+      max: 10,
       min: 0,
       acquire: 30000,
       idle: 10000,
